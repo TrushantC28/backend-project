b72d4de423eb8294396547b74b8710a788d1eeb5
Trushant <trushantchilvery11@gmail.com>
2026-08-14 12:17:19 +0530

    addition to atlas connection

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async () =>{
     try{
         const connection = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log("MongoDB connected\n")
-        console.log(`\n MongoDB connected ${connection.connection.host}`)
+        console.log(`${connection.connection.host}`)
 
     }catch(error){
         console.log("Error : ", error)
